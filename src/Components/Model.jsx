2d7820084b4d92dfd5647a6ccf8578fc6d98e951
Naomi Mugglestone <NJLM2708@outlook.com>
2022-12-07 02:55:07 +0000

    front end working, reconnected to mongo after updates and reinstalling

---
+++ src/Components/Model.jsx
@@ -0,0 +1,6 @@
+export default class Model {
+    constructor(postBody) {
+        this.postBody = postBody
+
+    }
+}
--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,7 +1,67 @@
-const NewPost = () => {
+import {useState} from "react";
+import axios from 'axios';
+import Model from './Model.jsx'
+
+const NewPost = ({}) => {
+
+const [newAddPost, setNewAddPost]= useState('');
+const [addPostMessage, setNewAddPostMessage] = useState('');
+
+const makeNewPost = async (e) => {
+e.preventDefault();
 
 
+const newPost = new Model(newAddPost)
 
+if(Object.keys(newPost).length){
+try {
+const res = await axios.post('http://localhost:3000/newPost/', newPost)
+setNewAddPostMessage(res.data.message);
+setNewAddPost('')
+}
+catch (err){
+setNewAddPostMessage('there are problems, try again later and it should work... hopefully')}
+}
 }
 
+
+    return (
+        <>
+            <div id="postComponent">
+                <div>
+                    <h1> Make a new Post </h1>
+                    <form onSubmit={makeNewPost}>
+                        <textarea
+                            onChange={e => setNewAddPost(e.target.value)} type="text" placeholder="Write here..." value={newAddPost} ></textarea>
+                        {addPostMessage && <small>{addPostMessage}</small>}
+                        <br />
+                        <input id="newPostButton" type="submit" value="Post" />
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+
+  }
 export default NewPost;
+
+
+
+
+// return (
+//         <>
+//             <div id="postComponent">
+//                 <div>
+//                     <h1> Make a new Post </h1>
+//                     <h2 className="postName">{firstName} {secondName}</h2>
+//                     <form onSubmit={makeNewPost}>
+//                         <textarea
+//                             onChange={e => setNewAddPost(e.target.value)} type="text" placeholder="Write here..." value={newAddPost} ></textarea>
+//                         {addPostMessage && <small>{addPostMessage}</small>}
+//                         <br />
+//                         <input id="newPostButton" type="submit" value="Post" />
+//                     </form>
+//                 </div>
+//             </div>
+//         </>
+//     )
